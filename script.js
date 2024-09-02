cf43726d95f82e6bd7e8b8aa23fb158176c74539
StepanidaK <steshakirillina@yandex.ru>
2024-09-02 09:02:49 +0900

    add new file

---
+++ script.js
@@ -0,0 +1,26 @@
+const numberOfFilms  = +prompt("Сколько фильмов вы уже посмотрели?")
+
+
+const personalMovieDB = {
+
+    count: numberOfFilms, 
+    movies: {}, 
+    actors: {},
+    genres: [],
+    private: false
+
+}
+
+const a = prompt("Один из последних просмотренных фильмов?");
+
+const b = +prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+
+// не используем dot notation, чтобы избежать возможных проблем при добавлении свойства в объект. 
+// Когда добавила a через точку в объект, то записалась, как a 
+
+console.log(personalMovieDB);
+
+
+
